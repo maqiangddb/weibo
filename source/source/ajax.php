d9a3fa52fd8e22eeb0965d4f0388d5bed182fec4
picasso250 <cumt.xiaochi@gmail.com>
2013-04-04 15:47:06 +0800

    delete comment

--- source/source/ajax.php
+++ source/source/ajax.php
@@ -7,7 +7,6 @@
  * @created Jul 18, 2012 10:23:53 PM
  */
 
-// print_r($role);
 $twits = Twit::search()->by('(UNIX_TIMESTAMP(`time`)+3*60) > UNIX_TIMESTAMP()')->find();
 echo json_encode(array_map(function ($t) {return $t->toArray();}, $twits));
 exit;
