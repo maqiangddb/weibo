9c7c73c9092a85f69afe67a71030944e53bda8e2
Ryan <cumt.xiaochi@gmail.com>
2013-10-01 13:56:11 +0800

    不需要什么登录用户
    好了，今天就改那么多，晚上回来继续。保持这种闲适而缓慢的心态。

--- source/controller/index.php
+++ source/controller/index.php
@@ -5,35 +5,11 @@
  * @created Jun 27, 2012 6:24:01 PM
  */
 
-class indexController
+class indexController extends Controller
 {
     public function indexAction()
     {
-        $twit_list = Twit::getListForIndex($per_page, $offset);
-
-        $twit_list = array_map(function ($t) use($user_id) {
-            $t['time'] = friendly_time2($t['time']);
-            if ($t['origin']) {
-                $t['origin']['time'] = friendly_time2($t['origin']['time']);
-            }
-            $twit = new Twit($t['id']);
-            $t['can_up'] = $twit->canUpBy($user_id);
-            $t['comments'] = $twit->getComments();
-            return $t;
-        }, $twit_list);
-
-        if ($has_login) {
-            $user = new User($user_id);
-            $reminds = $user->getReminds();
-            $reminds = array_filter($reminds, function ($r) {
-                return !in_array($r['id'], explode(',', get_set($_SESSION['se_visited_twits'])));
-            });
-            $reminds_count = $reminds? count($reminds) : 0;
-        }
-
-        $page['scripts'][] = js_node('jquery.form');
-        $page['scripts'][] = js_node('widget');
-        $page['style'] = 'index';
-
+        $this->twits = Twit::getListForIndex($per_page, $offset);
+        $this->rendView();
     }
 }
