3f087df242ebe9cbe006bf1f8166066529b42051
Ryan <cumt.xiaochi@gmail.com>
2013-10-01 13:52:28 +0800

    继承的基类是Model
    每个Twit出厂时就适宜网页显示

--- source/model/Twit.php
+++ source/model/Twit.php
@@ -9,31 +9,15 @@
  * @author  ryan <cumt.xiaochi@gmail.com>
  * @created Jul 17, 2012 3:15:17 PM
  */
-class Twit extends CoreModel {
+class Twit extends Model {
 
     protected static $table = 'twit';
 
     public function getComments() {
-        //....
-        $fields = array(
-            'comment.text',
-            'comment.time',
-            'role.name as author',
-            'role.id as author_id',
-            'role.avatar',
-            'role.is_v',
-        );
-        $tables = array('twit', 'comment', 'role');
-        $conds = array(
-            "twit.id=?"=>$this->id,
-            'twit.id=comment.twit'=>false,
-            'role.id=comment.author'=>false,
-        );
-        $orders = array('`comment`.`time` ASC');
-        return array_map(function ($comment) {
-            $comment['text'] = Twit::formatHtml($comment['text']);
-            return $comment;
-        }, Pdb::fetchAll($fields, $tables, $conds, $orders));
+        return Comment::search()
+            ->where('twit_id', $this->id())
+            ->order(array('id' => 'ASC'))
+            ->findMany();
     }
 
     public static function formatHtml($text) { // this should be private, but...
@@ -124,7 +108,13 @@ class Twit extends CoreModel {
             ->findMany();
 
         foreach($ret as $k => &$tw) {
-            $tw->text = self::formatHtml($tw->text);
+            
+                if ($t['origin']) {
+                    $t['origin']['time'] = friendly_time2($t['origin']['time']);
+                }
+                $twit = new Twit($t['id']);
+                $t['can_up'] = $twit->canUpBy($user_id);
+                return $t;
         }
         return $ret;
     }
@@ -135,7 +125,32 @@ class Twit extends CoreModel {
         if ($o->origin) {
             $o->orgin = self::findOne($arr);
         }
+
+        $o->text = self::formatHtml($o->text);
+        $o->time = self::readableTime($o->time);
+        $o->comments = $o->getComments();
         return $o;
+    }
+
+    /** translate Y-m-d to xx之前 or 今天XX
+     *
+     * @param type $date_time_str 形如 Y-m-d H:i:s （sql中获得的DateTime类型即可）
+     */
+    public function readableTime($date_time_str) {
+        $date_time = new DateTime($date_time_str);
+        $nowtime = new DateTime();
+        $diff = $nowtime->diff($date_time);
+        if ($diff->y==0 && $diff->m==0 && $diff->d==0) { // 同一天
+            if ($diff->h<1) // 一个小时以内
+                if ($diff->i==0) // 一分钟以内
+                    return '刚刚';
+                else
+                    return $diff->i.'分钟前'; // minutes
+            else
+                return '今天';
+        } else {
+            return current(explode(' ', $date_time_str));
+        }
     }
 }
 
