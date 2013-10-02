7dd6369ae7135936ee994753f316e8aeca6f433e
Ryan <cumt.xiaochi@gmail.com>
2013-10-02 13:45:32 +0800

    在vendor里面？

--- source/autoload.php
+++ source/autoload.php
@@ -2,10 +2,9 @@
 
 spl_autoload_register(function ($name) {
     if (preg_match('/^ptf\b/', $name)) {
-        $file = __DIR__.str_replace('\\', '/', $name).'.php';
+        $file = __DIR__.'/vender/'.str_replace('\\', '/', $name).'.php';
         require $file;
     }
-    
 });
 
-require __DIR__.'/vendor/autoload.php';
+// require __DIR__.'/vendor/autoload.php';
