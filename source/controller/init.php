b230dcaab4ee96e00ecd28a0b6b2b62022ebd1c1
Ryan <cumt.xiaochi@gmail.com>
2013-10-02 10:31:29 +0800

    需要一个基类，做controller的初始化动作

--- source/controller/init.php
+++ source/controller/init.php
@@ -1,54 +1,50 @@
 <?php
-!defined('IN_KC') && exit('Access Denied');
 /**
  * @file    init
  * @author  ryan <cumt.xiaochi@gmail.com>
  * @created Jun 30, 2012 11:50:49 AM
  */
 
-$page['description'] = '伪博扮演，扮演整个世界';
-$page['keywords'] = array('伪博','扮演');
+class baseController extends Controller
+{
+    public function __construct()
+    {
+        $this->page = new stdClass;
+        $this->page->description = '伪博扮演，扮演整个世界';
+        $this->page->keywords = array('伪博','扮演');
 
-$rooturl = ROOT; // TODO
-$method = get_set($_REQUEST['method']);
+        ORM::configure($config['db']);
 
-Pdb::setConfig($config['pdb']);
-ORM::configure($config['pdb']['dsn']);
-ORM::configure('username', $config['pdb']['username']);
-ORM::configure('password', $config['pdb']['password']);
-if (DEBUG) {
-    ORM::configure('logging', true);
-}
+        $role_id = get_set($_SESSION['se_role_id']);
+        if ($role_id) {
+            $role = new Role($role_id);
+            $role_info = $role->getInfo();
+            fill_empty($role_info['avatar'], $config['default_avatar']);
+        }
 
-require_once AROOT.'lib/db.function.php';
-require_once AROOT.'lib/core.class.php';
+        $has_login = 0; // 那就总是lognin？？
+        $user_id = get_set($_SESSION['se_user_id']);
+        if ($user_id && isset($_SESSION['se_user_name'])) {
+            $has_login = 1;
+            $user = new User($user_id);
+            $user_info = $user->getInfo();
+        } else {
+            $cookie_id = get_set($_COOKIE['xc_id']);
+            if (!$cookie_id) {
+                $cookie_id = md5(uniqid());
+                setcookie('xc_id', $cookie_id, time() + 3600*24*180); // 半年
+            }
+            $platform = 'cookie';
 
-$role_id = get_set($_SESSION['se_role_id']);
-if ($role_id) {
-    $role = new Role($role_id);
-    $role_info = $role->getInfo();
-    fill_empty($role_info['avatar'], $config['default_avatar']);
-}
+            $user = User::get($platform, $cookie_id);
+            if ($user === false) { // if not exist
+                $user = User::createFromOpenId($platform, $cookie_id);
+            }
+            $user_id = $_SESSION['se_user_id'] = $user->getId(); // 确保 $user_id有值
+        }
 
-$has_login = 0; // 那就总是lognin？？
-$user_id = get_set($_SESSION['se_user_id']);
-if ($user_id && isset($_SESSION['se_user_name'])) {
-    $has_login = 1;
-    $user = new User($user_id);
-    $user_info = $user->getInfo();
-} else {
-    $cookie_id = get_set($_COOKIE['xc_id']);
-    if (!$cookie_id) {
-        $cookie_id = md5(uniqid());
-        setcookie('xc_id', $cookie_id, time() + 3600*24*180); // 半年
+        $perm = Perm::getByUserKind(get_set($user_info['kind']));
+
     }
-    $platform = 'cookie';
-
-    $user = User::get($platform, $cookie_id);
-    if ($user === false) { // if not exist
-        $user = User::createFromOpenId($platform, $cookie_id);
-    }
-    $user_id = $_SESSION['se_user_id'] = $user->getId(); // 确保 $user_id有值
 }
 
-$perm = Perm::getByUserKind(get_set($user_info['kind']));
